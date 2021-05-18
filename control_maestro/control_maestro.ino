f353501caf0dfd779c963457579c286294d8e751
PeteAle <>
2021-05-18 16:36:03 -0600

    Inicialización de constantes
    Se inicializaron las constantes para cada uno de los controladores PID

---
+++ control_maestro/control_maestro.ino
@@ -0,0 +1,48 @@
+// Se definene las librerías a utilizar
+
+#include <EEPROM.h>
+
+#define 
+#define 
+
+// Constantes del control 1
+float kp1 = 0;
+float ki1 = 0;
+float kd1 = 0;
+
+// Constates del control 2
+float kp2 = 0;
+float ki2 = 0;
+float kd2 = 0;
+
+// Constantes del control 3
+float kp3 = 0;
+float ki3 = 0;
+float kd3 = 0;
+
+// Constates del control 4
+float kp4 = 0;
+float ki4 = 0;
+float kd4 = 0;
+
+// Constantes del control 5
+float kp5 = 0;
+float ki5 = 0;
+float kd5 = 0;
+
+// Constates del control 6
+float kp6 = 0;
+float ki6 = 0;
+float kd6 = 0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
